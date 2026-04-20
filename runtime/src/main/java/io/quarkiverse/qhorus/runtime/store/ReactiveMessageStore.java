50a66a6da3d553ec917542ba05db9abd3df85890
Mark Proctor <mdproctor@gmail.com>
2026-04-20 19:31:12 +0100

    feat(store): Reactive*Store interfaces for all 5 domains
    Refs #74

---
+++ runtime/src/main/java/io/quarkiverse/qhorus/runtime/store/ReactiveMessageStore.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.qhorus.runtime.store;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import io.quarkiverse.qhorus.runtime.message.Message;
+import io.quarkiverse.qhorus.runtime.store.query.MessageQuery;
+import io.smallrye.mutiny.Uni;
+
+public interface ReactiveMessageStore {
+    Uni<Message> put(Message message);
+
+    Uni<Optional<Message>> find(Long id);
+
+    Uni<List<Message>> scan(MessageQuery query);
+
+    Uni<Void> deleteAll(UUID channelId);
+
+    Uni<Void> delete(Long id);
+
+    Uni<Integer> countByChannel(UUID channelId);
+}
